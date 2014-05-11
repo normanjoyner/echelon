ad194cfaaf778e160578f1ea85cdf5aaddeddf68
Norman Joyner <norman.joyner@gmail.com>
2014-05-11 14:13:18 -0400

    Add main echelon file

---
+++ echelon.js
@@ -0,0 +1,40 @@
+var _ = require("lodash");
+var queue = require([__dirname, "lib", "queue"].join("/"));
+var handlers = require([__dirname, "lib", "handlers"].join("/"));
+var espial = require([__dirname, "lib", "espial"].join("/"));
+
+// define Echelon
+function Echelon(){}
+
+// initialize Echelon
+Echelon.prototype.init = function(options, fn){
+    queue.init();
+    espial.init(this, fn);
+}
+
+// add job handler
+Echelon.prototype.add_handler = function(name, handler){
+    handlers.add(name, handler);
+}
+
+// remove job handler
+Echelon.prototype.remove_handler = function(name){
+    handlers.remove(name);
+}
+
+// add job to the queue
+Echelon.prototype.add_job = function(job){
+    // if we're the master node, add the job to the queue
+    if(this.espial.is_master())
+        queue.push(job);
+
+    // if find a master, send the job to them
+    else if(!_.isEmpty(this.espial.get_master()))
+        espial.cluster.send("add_job", job, this.espial.get_master());
+
+    // hold the job until we find a master
+    else
+        queue.stash(job);
+}
+
+module.exports = Echelon;
